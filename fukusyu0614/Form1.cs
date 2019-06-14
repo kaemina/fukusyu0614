f9c0bd2e3ec31ee50f5c4500cdf427e60e9170f4
Yuki <>
2019-06-14 13:27:20 +0900

    提出

--- fukusyu0614/Form1.cs
+++ fukusyu0614/Form1.cs
@@ -12,9 +12,29 @@ namespace fukusyu0614
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if(label1.Left<=0)
+            {
+                vx = -vx;
+            }
+          if(label1.Top<=0)
+          {
+              vy = -vy;
+          }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
